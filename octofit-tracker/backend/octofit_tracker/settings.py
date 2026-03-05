87b5242677b50027be2036c7fb9d50e8eacc1977
Bogdan Drozdowski <93281795+bogdro@users.noreply.github.com>
2026-03-05 14:30:26 +0000

    Update urls

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-x76%_q(t%b&d-y6-9drzm+8dj73zq3h6+kzv$hf^2vt$r@2+%=
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow codespace and localhost hosts
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
